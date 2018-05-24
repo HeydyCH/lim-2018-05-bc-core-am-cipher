31fe6f3994acf499362981d843bc0c8b9f23702f
HeydyCH <heidich8@gmail.com>
2018-05-24 09:13:14 -0500

    Ejemplo basico de conexion de index.js y cipher.js

--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,13 @@
 window.cipher = {
-  // ... 
+
+  encode :(offset,string)=>{
+    var resultEncode = string + offset;
+    return resultEncode;
+  } ,
+
+  decode :(offset,string)=>{
+     var resultDecode = offset + string;
+    return resultDecode;
+  }
+
 };
---
+++ src/index.js
@@ -0,0 +1,3 @@
+
+console.log(cipher.encode(33,"A"));
+console.log(cipher.decode(33,"K"));
