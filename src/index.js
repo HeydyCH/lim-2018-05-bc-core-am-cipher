aa68674454490c136bf84545b5ceb372b2ac0379
HeydyCH <heidich8@gmail.com>
2018-06-03 23:12:04 -0500

    Resposive y modificacion de restriccion de campos

--- src/index.js
+++ src/index.js
@@ -10,36 +10,27 @@ let result = "";
 function resultCipher(){
   string=document.getElementById("text").value;
   offset=document.getElementById("position").value;
-  if(offset >0 && string!=""){
-    result=cipher.createCipherWithOffset(offset).encode(string);
-    resultMessage.innerHTML = result; 
-  }else if(string == ""){
-    document.getElementById("warning").innerHTML = "Error !! No ingreso el texto a Cifrar ";
-  }else if(offset == 0 ){
-    document.getElementById("warning").innerHTML = "Error !! No ingreso el numero ";
-  }else if(offset <= 0){
-    document.getElementById("warning").innerHTML = "Error !! Ingrese un nro mayor que 0  ";
+  if(offset<1){
+    alert("Ingrese un nro mayor que 0");
+  }else if(string==""){
+    alert("Ingrese el texto a encriptar");
   }else{
-    document.getElementById("warning").innerHTML = "Error !!  ";
+    result=cipher.createCipherWithOffset(offset).encode(string);
+    resultMessage.innerHTML = result;
   }
 }
 
 function resultDecipher(){
   string=document.getElementById("text").value;
   offset=document.getElementById("position").value;
-
-  if(offset >0 && string!=""){
+  if(offset<1){
+    alert("Ingrese un nro mayor que 0");
+  }else if(string==""){
+    alert("Ingrese el texto a desincriptar");
+  }else{
     result =cipher.createCipherWithOffset(offset).decode(string)
     resultMessage.innerHTML = result;
-  }else if(string == ""){
-    resultMessage.innerHTML = "Error !! No ingreso el texto a Cifrar "; 
-  }else if(offset == 0 ){
-    resultMessage.innerHTML = "Error !! No ingreso el numero "; 
-  }else if(offset <= 0){
-    resultMessage.innerHTML = "Error !! Ingrese un nro mayor que 0  ";
-  }else{
-    resultMessage.innerHTML = "Error !!"; 
-  }  
+  } 
 }
 
 function cleanField(){
